873e92035b76482d9e80f7bdaf5c0ad87afba433
Masato-Nishikubo <c611801517@tokushima-u.ac.jp>
2019-11-29 17:24:38 +0900

    名前入力機能の変更、クライアント側のネットワーク部分の改良

--- data/src/client/main/Game.cpp
+++ data/src/client/main/Game.cpp
@@ -1,198 +1,107 @@
 #include "Game.h"
-#include "./Client_window.h"
-#include "../actor/Racer.h"
-#include <algorithm>
-
 
 Game::Game()
-	: mEndFlag(1), mUpdatingActors(false)
+    : mEndFlag(1)
 {
 }
 
+int Game::clientID;
+
 bool Game::Initialize(int argc, char *argv[])
 {
-	char name[MAX_CLIENTS][MAX_NAME_SIZE];
-	char localHostName[] = "localhost";
-	char *serverName;
-	int clientID;
-	class Client_command *command;
+    char name[MAX_CLIENTS][MAX_NAME_SIZE];
+    char localHostName[] = "localhost";
+    char *serverName;
 
-	mNet = new Client_net(this);
-	mWindow = new Client_window(this);
-	mCommand = new Client_command(this);
+    class Client_command *command;
 
-	/* 引き数チェック */
-	if (argc == 1)
-	{
-		serverName = localHostName;
-	}
-	else if (argc == 2)
-	{
-		serverName = argv[1];
-	}
-	else
-	{
-		fprintf(stderr, "Usage: %s, Cannot find a Server Name.\n", argv[0]);
-		return -1;
-	}
+    mNet = new Client_net(this);
+    mWindow = new Client_window(this);
+    mCommand = new Client_command(this);
 
-	/* サーバーとの接続 */
-	if (mNet->SetUpClient(serverName, &clientID, &mNum, name) == -1)
-	{
-		fprintf(stderr, "Setup failed : SetUpClient\n");
-		return -1;
-	}
-	/* ウインドウの初期化 */
-	if (!mWindow->InitWindows(clientID, mNum, name))
-	{
-		fprintf(stderr, "Setup failed : InitWindows\n");
-		return -1;
-	}
+    /* 引き数チェック */
+    if (argc == 1)
+    {
+        serverName = localHostName;
+    }
+    else if (argc == 2)
+    {
+        serverName = argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s, Cannot find a Server Name.\n", argv[0]);
+        return -1;
+    }
 
-	/* ネットワークイベント処理スレッドの作成 */
-	//thr = SDL_CreateThread(Game::NetworkEvent,"NetworkThread" ,&mEndFlag);
+    /* サーバーとの接続 */
+    if (mNet->SetUpClient(serverName, &clientID, &mNum, name) == -1)
+    {
+        fprintf(stderr, "setup failed : SetUpClient\n");
+        return -1;
+    }
+    /* ウインドウの初期化 */
+    if (!mWindow->InitWindows(clientID, mNum, name))
+    {
+        fprintf(stderr, "setup failed : InitWindows\n");
+        return -1;
+    }
 
-	mTicksCount = SDL_GetTicks();
+    /* ネットワークイベント処理スレッドの作成 */
+    thr = SDL_CreateThread(NetworkEvent, "NetworkThread", &mEndFlag);
 
-	class Racer* player = new Racer(this);
+    mTicksCount = SDL_GetTicks();
 
-	return true;
+    return true;
 }
 
 void Game::RunLoop()
 {
-	ProcessInput();
-	UpdateGame();
-	GenerateOutput();
+    ProcessInput();
+    UpdateGame();
+    GenerateOutput();
 }
 
 void Game::Shutdown()
 {
-	/* 終了処理 */
-	SDL_WaitThread(thr, NULL);
-	mWindow->DestroyWindow();
-	mNet->CloseSoc();
+    /* 終了処理 */
+    SDL_WaitThread(thr, NULL);
+    mWindow->DestroyWindow();
+    mNet->CloseSoc();
 
-	SDL_Quit();
+    SDL_Quit();
 }
 
 //private
 
 void Game::ProcessInput()
 {
-	NetworkEvent(&mEndFlag);
+    // 試験的な実装
+    mWindow->WindowEvent(mNum);
 
-	SDL_Event event;
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-			case SDL_QUIT:
-				mEndFlag = false;
-				break;
-		}
-	}
-	
-	const Uint8* keyState = SDL_GetKeyboardState(NULL);
-
-	mUpdatingActors = true;
-	for (auto actor : mActors)
-	{
-		actor->ProcessInput(keyState);
-	}
-	mUpdatingActors = false;
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    //if escape is pressed, also end loop
+    if (state[SDL_SCANCODE_UP])
+    {
+        mCommand->SendPosCommand();
+    }
 }
 
 void Game::UpdateGame()
 {
-	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16))
-		;
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16))
+        ;
 
-	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
-	if (deltaTime > 0.05f)
-	{
-		deltaTime = 0.05f;
-	}
-	mTicksCount = SDL_GetTicks();
-
-	// Update all actors
-	mUpdatingActors = true;
-	for (auto actor : mActors)
-	{
-		actor->Update(deltaTime);
-	}
-	mUpdatingActors = false;
-
-	// Move any pending actors to mActors
-	for (auto pending : mPendingActors)
-	{
-		mActors.emplace_back(pending);
-	}
-	mPendingActors.clear();
-
-	// Add any dead actors to a temp vector
-	std::vector<Actor *> deadActors;
-	for (auto actor : mActors)
-	{
-		if (actor->GetState() == Actor::EDead)
-		{
-			deadActors.emplace_back(actor);
-		}
-	}
-
-	for (auto actor : deadActors)
-	{
-		delete actor;
-	}
-}
-
-void Game::AddActor(Actor *actor)
-{
-	// If we're updating actors, need to add to pending
-	if (mUpdatingActors)
-	{
-		mPendingActors.emplace_back(actor);
-	}
-	else
-	{
-		mActors.emplace_back(actor);
-	}
-}
-
-void Game::RemoveActor(Actor *actor)
-{
-	// Is it in pending actors?
-	auto iter = std::find(mPendingActors.begin(), mPendingActors.end(), actor);
-	if (iter != mPendingActors.end())
-	{
-		// Swap to end of vector and pop off (avoid erase copies)
-		std::iter_swap(iter, mPendingActors.end() - 1);
-		mPendingActors.pop_back();
-	}
-
-	// Is it in actors?
-	iter = std::find(mActors.begin(), mActors.end(), actor);
-	if (iter != mActors.end())
-	{
-		// Swap to end of vector and pop off (avoid erase copies)
-		std::iter_swap(iter, mActors.end() - 1);
-		mActors.pop_back();
-	}
-}
-
-void Game::AddSprite(SpriteComponent *sprite)
-{
-	mWindow -> AddSprite(sprite);
-}
-
-void Game::RemoveSprite(SpriteComponent *sprite)
-{
-	mWindow -> RemoveSprite(sprite);
+    float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+    if (deltaTime > 0.05f)
+    {
+        deltaTime = 0.05f;
+    }
+    mTicksCount = SDL_GetTicks();
 }
 
 void Game::GenerateOutput()
 {
-	mWindow -> Draw();
 }
 
 /*****************************************************************
@@ -203,11 +112,12 @@ void Game::GenerateOutput()
 *****************************************************************/
 int Game::NetworkEvent(void *data)
 {
-	int *endFlag;
+    int *endFlag;
 
-	endFlag = (int *)data;
-	//while(*endFlag){
-	*endFlag = mNet->SendRecvManager();
-	//}
-	return 0;
+    endFlag = (int *)data;
+    while (*endFlag)
+    {
+        *endFlag = Client_net::SendRecvManager();
+    }
+    return 0;
 }
--- data/src/server/main/Server_net.cpp
+++ data/src/server/main/Server_net.cpp
@@ -1,15 +1,16 @@
-#include "Game.h"
+
 #include "Server_common.h"
-#include"Server_func.h"
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
+#include "Server_func.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 Server_net::Server_net()
@@ -17,49 +18,69 @@ Server_net::Server_net()
     mServer_command = new Server_command(this);
 }
 
+/*変数の宣言*/
+CLIENT Server_net::mClients[MAX_CLIENTS];
+int Server_net::endFlag = 1;
+int Server_net::gClientNum;
+int Server_net::gWidth;
+fd_set Server_net::gMask;
+timeval Server_net::tv;
+bool Server_net::running;
+static int request_soc;
+
 int Server_net::SetUpServer(int num)
 {
-    struct sockaddr_in	server;
-    int			request_soc;
-    int                 maxfd;
-    int			val = 1;
- 
+    struct sockaddr_in server;
+
+    int maxfd;
+    int val = 1;
+
     /* 引き数チェック */
-    assert(0<num && num<=MAX_CLIENTS);
+    assert(0 < num && num <= MAX_CLIENTS);
 
     gClientNum = num;
-    bzero((char*)&server,sizeof(server));
+    bzero((char *)&server, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = htonl(INADDR_ANY);
 
     /* ソケットを作成する */
-    if((request_soc = socket(AF_INET,SOCK_STREAM,0)) < 0){
-		fprintf(stderr,"Socket allocation failed\n");
-		return -1;
+    if ((request_soc = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        fprintf(stderr, "Socket allocation failed\n");
+        return -1;
     }
-    setsockopt(request_soc,SOL_SOCKET,SO_REUSEADDR,&val,sizeof(val));
+    setsockopt(request_soc, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
     /* ソケットに名前をつける */
-    if(bind(request_soc,(struct sockaddr*)&server,sizeof(server))==-1){
-		fprintf(stderr,"Cannot bind\n");
-		close(request_soc);
-		return -1;
+    if (bind(request_soc, (struct sockaddr *)&server, sizeof(server)) == -1)
+    {
+        fprintf(stderr, "Cannot bind\n");
+        close(request_soc);
+        return -1;
     }
-    fprintf(stderr,"Successfully bind!\n");
-    
+    fprintf(stderr, "Successfully bind!\n");
+
     /* クライアントからの接続要求を待つ */
-    if(listen(request_soc, gClientNum) == -1){
-		fprintf(stderr,"Cannot listen\n");
-		close(request_soc);
-		return -1;
+    if (listen(request_soc, gClientNum) == -1)
+    {
+        fprintf(stderr, "Cannot listen\n");
+        close(request_soc);
+        return -1;
     }
-    fprintf(stderr,"Listen OK\n");
+    fprintf(stderr, "Listen OK\n");
 
     /* クライアントと接続する */
     maxfd = MultiAccept(request_soc, gClientNum);
     close(request_soc);
-    if(maxfd == -1)return -1;
+
+    if (maxfd == -1)
+        return -1;
+
+    struct timeval tv;
+
+    tv.tv_sec = 0;
+    tv.tv_usec = 20;
 
     /* 全クライアントの全ユーザー名を送る */
     SendAllName();
@@ -72,134 +93,157 @@ int Server_net::SetUpServer(int num)
 
 int Server_net::SendRecvManager()
 {
-    char	command;
-    fd_set	readOK;
-    int		i;
-    int		endFlag = 1;
+    int i;
+    int retval;
+    fd_set readOK;
 
     readOK = gMask;
     /* クライアントからデータが届いているか調べる */
-    if(select(gWidth,&readOK,NULL,NULL,NULL) < 0){
+    retval = select(gWidth, &readOK, NULL, NULL, &tv);
+    if (retval == -1)
+    {
         /* エラーが起こった */
         return endFlag;
     }
+    else if (retval)
+    {
+        for (i = 0; i < gClientNum; i++)
+        {
+            if (FD_ISSET(mClients[i].fd, &readOK))
+            {
 
-    for(i=0;i<gClientNum;i++){
-		if(FD_ISSET(mClients[i].fd,&readOK)){
-	    	/* クライアントからデータが届いていた */
-	    	/* コマンドを読み込む */
-			RecvData(i,&command,sizeof(char));
-	    	/* コマンドに対する処理を行う */
-	    	endFlag = mServer_command->ExecuteCommand(command,i);
-	    	if(endFlag == 0)break;
-		}
+                /* コマンドに対する処理を行う */
+                endFlag = Server_command::ExecuteCommand(i);
+            }
+            if (endFlag == 0)
+                break;
+        }
     }
+
     return endFlag;
 }
 
-int Server_net::RecvIntData(int pos,int *intData)
+void Server_net::SendData(int pos, void *data, int dataSize)
 {
-    int n,tmp;
-    
+    int i;
+
     /* 引き数チェック */
-    assert(0<=pos && pos<gClientNum);
-    assert(intData!=NULL);
+    assert(0 <= pos && pos < gClientNum || pos == ALL_CLIENTS);
+    assert(data != NULL);
+    assert(0 < dataSize);
 
-    n = RecvData(pos,&tmp,sizeof(int));
-    (*intData) = ntohl(tmp);
-    
-    return n;
-}
-
-void Server_net::SendData(int pos,void *data,int dataSize)
-{
-    int	i;
-   
-    /* 引き数チェック */
-    assert(0<=pos && pos<gClientNum || pos==ALL_CLIENTS);
-    assert(data!=NULL);
-    assert(0<dataSize);
-
-    if(pos == ALL_CLIENTS){
-    	/* 全クライアントにデータを送る */
-		for(i=0;i<gClientNum;i++){
-			write(mClients[i].fd,data,dataSize);
-		}
+    if (pos == ALL_CLIENTS)
+    {
+        /* 全クライアントにデータを送る */
+        for (i = 0; i < gClientNum; i++)
+        {
+            write(mClients[i].fd, data, dataSize);
+        }
     }
-    else{
-		write(mClients[pos].fd,data,dataSize);
+    else
+    {
+        write(mClients[pos].fd, data, dataSize);
     }
 }
 
 void Server_net::Ending(void)
 {
-    int	i;
+    int i;
+
+    SDL_WaitThread(thr, NULL);
 
     printf("... Connection closed\n");
-    for(i=0;i<gClientNum;i++)close(mClients[i].fd);
+    for (i = 0; i < gClientNum; i++)
+        close(mClients[i].fd);
 }
 
-int Server_net::MultiAccept(int request_soc,int num)
+int Server_net::MultiAccept(int request_soc, int num)
 {
-    int	i,j;
-    int	fd;
-    
-    for(i=0;i<num;i++){
-		if((fd = accept(request_soc,NULL,NULL)) == -1){
-			fprintf(stderr,"Accept error\n");
-			for(j=i-1;j>=0;j--)close(mClients[j].fd);
-			return -1;
-		}
-		Enter(i,fd);
+    int i, j;
+    int fd;
+
+    for (i = 0; i < num; i++)
+    {
+        if ((fd = accept(request_soc, NULL, NULL)) == -1)
+        {
+            fprintf(stderr, "Accept error\n");
+            for (j = i - 1; j >= 0; j--)
+                close(mClients[j].fd);
+            return -1;
+        }
+        else
+        {
+            /* スレッドの作成*/
+            thr = SDL_CreateThread(NetworkEvent, "NetworkThread", &endFlag);
+            Enter(i, fd);
+        }
     }
     return fd;
 }
 
 void Server_net::Enter(int pos, int fd)
 {
-	/* クライアントのユーザー名を受信する */
-	read(fd,mClients[pos].name,MAX_NAME_SIZE);
-#ifndef NDEBUG
-	printf("%s is accepted\n",mClients[pos].name);
-#endif
-	mClients[pos].fd = fd;
+
+    printf("プレイヤーID%dさんが入室しました\n", pos);
+
+    mClients[pos].fd = fd;
 }
 
 void Server_net::SetMask(int maxfd)
 {
-    int	i;
+    int i;
 
-    gWidth = maxfd+1;
+    gWidth = maxfd + 1;
 
-    FD_ZERO(&gMask);    
-    for(i=0;i<gClientNum;i++)FD_SET(mClients[i].fd,&gMask);
+    FD_ZERO(&gMask);
+    for (i = 0; i < gClientNum; i++)
+        FD_SET(mClients[i].fd, &gMask);
 }
 
 void Server_net::SendAllName(void)
 {
-  int	i,j,tmp1,tmp2;
+    int i, j, tmp1, tmp2;
 
     tmp2 = htonl(gClientNum);
-    for(i=0;i<gClientNum;i++){
-		tmp1 = htonl(i);
-		SendData(i,&tmp1,sizeof(int));
-		SendData(i,&tmp2,sizeof(int));
-		for(j=0;j<gClientNum;j++){
-			SendData(i,mClients[j].name,MAX_NAME_SIZE);
-		}
-	}
+    for (i = 0; i < gClientNum; i++)
+    {
+        tmp1 = htonl(i);
+        SendData(i, &tmp1, sizeof(int));
+        SendData(i, &tmp2, sizeof(int));
+        for (j = 0; j < gClientNum; j++)
+        {
+            SendData(i, mClients[j].name, MAX_NAME_SIZE);
+        }
+    }
 }
 
-int Server_net::RecvData(int pos,void *data,int dataSize)
+int Server_net::RecvData(int pos, void *data, int dataSize)
 {
-    int n;
-    
-    /* 引き数チェック */
-    assert(0<=pos && pos<gClientNum);
-    assert(data!=NULL);
-    assert(0<dataSize);
 
-    n = read(mClients[pos].fd,data,dataSize);
-    
-    return n;
+    assert(0 <= pos && pos < gClientNum);
+    assert(data != NULL);
+    assert(0 < dataSize);
+
+    return read(mClients[pos].fd, data, dataSize);
+}
+
+/*****************************************************************
+関数名  : NetworkEvent
+機能    : ネットワークのイベントループ
+引数    : void		*data		: 終了判定フラグ
+出力    : スレッド終了時に０を返す
+*****************************************************************/
+int Server_net::NetworkEvent(void *data)
+{
+    int *endflag;
+    running = true;
+
+    endflag = (int *)data;
+    while (*endflag)
+    {
+        *endflag = SendRecvManager();
+    }
+    printf("スレッドの終了\n");
+    running = false;
+    return 0;
 }
