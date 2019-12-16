3890c2cd9c3675f171d0396a970701e2e927714a
Miki-Tatsuya <mt81km@gmail.com>
2019-12-16 16:16:02 +0900

    ミニマップの追加

--- data/src/client/ui/HUD.cpp
+++ data/src/client/ui/HUD.cpp
@@ -4,6 +4,9 @@
 #include "../actor/Racer.h"
 #include "../main/Client_func.h"
 #include "../../common/math/Math.h"
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <algorithm>
 
 HUD::HUD(Game* game)
@@ -14,19 +17,25 @@ HUD::HUD(Game* game)
         switch(i){
             case 0 : mRankingUI[i] = mGame->GetClient_window()->GetTexture("assets/images/RankOne.png");
                      mRacerUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Player_red.png");
+                     mPlayerPosUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Pl1pos.png");
                      break;
             case 1 : mRankingUI[i] = mGame->GetClient_window()->GetTexture("assets/images/RankTwo.png");
                      mRacerUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Player_blue.png");
+                     mPlayerPosUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Pl2pos.png");
                      break;
             case 2 : mRankingUI[i] = mGame->GetClient_window()->GetTexture("assets/images/RankThree.png");
                      mRacerUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Player_green.png");
+                     mPlayerPosUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Pl3pos.png");
                      break;
             case 3 : mRankingUI[i] = mGame->GetClient_window()->GetTexture("assets/images/RankFour.png");
                      mRacerUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Player_purple.png");
+                     mPlayerPosUI[i] = mGame->GetClient_window()->GetTexture("assets/images/Pl4pos.png");
                      break;
         }
     }
     mNoPlayerUI = mGame->GetClient_window()->GetTexture("assets/images/Player_nothing.png");
+    mMapUI = mGame->GetClient_window()->GetTexture("assets/images/StageRoad.png");
+    mMapBaseUI = mGame->GetClient_window()->GetTexture("assets/images/StageUIBase.png");
     mWindowWidth = mGame->GetClient_window()->GetWidth();
     mWindowHeight = mGame->GetClient_window()->GetHeight();
 }
@@ -40,21 +49,47 @@ void HUD::Update(float deltaTime)
 	Canvas::Update(deltaTime);
 	
 	UpdateRanking();
+    PlayerPosUpdate();
 }
 
 void HUD::Draw(SDL_Renderer* renderer)
 {
+    // ランキングの描画
+    float uiSize = (float)mWindowHeight / 1000;
+    int w, h;
+    SDL_QueryTexture(mRankingUI[0], NULL, NULL, &w, &h);
     Vector2 uiPos = Vector2::Zero;
-    uiPos.y += mWindowHeight - 50;
+    uiPos.y += mWindowHeight - ((float)h * uiSize);
     for(int i = 0; i < MAX_CLIENTS; i++)
     {
-        DrawTexture(renderer, mRankingUI[i], uiPos, 0.5);
-        uiPos.x += 50;
+        DrawTexture(renderer, mRankingUI[i], uiPos, uiSize);
+        uiPos.x += (float)w * uiSize;
         if (mRank[i]>0)
-            DrawTexture(renderer, mRacerUI[mRank[i] - 1], uiPos, 0.5);
+            DrawTexture(renderer, mRacerUI[mRank[i] - 1], uiPos, uiSize);
         else
-            DrawTexture(renderer, mNoPlayerUI, uiPos, 0.5);
-        uiPos.x += 50;
+            DrawTexture(renderer, mNoPlayerUI, uiPos, uiSize);
+        uiPos.x += (float)w * uiSize;
+    }
+    // ミニマップの描画
+    int w2, h2;
+    uiSize = (float)mWindowHeight / 2500;
+    SDL_QueryTexture(mMapBaseUI, NULL, NULL, &w, &h);
+    SDL_QueryTexture(mMapUI, NULL, NULL, &w2, &h2);
+    uiPos = Vector2::Zero;
+    DrawTexture(renderer, mMapBaseUI, uiPos, uiSize);
+    DrawTexture(renderer, mMapUI, uiPos, uiSize);
+    // ミニマップにプレイヤを描画
+    SDL_QueryTexture(mPlayerPosUI[0], NULL, NULL, &w, &h);
+    uiPos.x += (((float)w2 * uiSize) / 2);
+    uiPos.y += (((float)h2 * uiSize) / 2);
+    float plUISize = uiSize/2;
+    Vector2 plUIPos = Vector2::Zero;
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        plUIPos = uiPos;
+        plUIPos.x += ((mPlPos[i].x / (10/uiSize))-(w*plUISize/2));  // 10はステージのミニマップに対する画像倍率
+        plUIPos.y += ((mPlPos[i].y / (10/uiSize))-(h*plUISize/2));
+        DrawTexture(renderer, mPlayerPosUI[i], plUIPos, plUISize);
     }
 }
 
@@ -74,5 +109,13 @@ void HUD::UpdateRanking()
     for (int i = 0; i < MAX_CLIENTS; i++)
     {
         mRank[i] = mGame->GetClient_command()->PlayerPos[i].rank;
+    }
+}
+
+void HUD::PlayerPosUpdate()
+{
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        mPlPos[i] = mGame->mRacer[i]->GetPosition();
     }
 }
--- data/src/client/ui/HUD.h
+++ data/src/client/ui/HUD.h
@@ -23,16 +23,22 @@ public:
 
   protected:
     void UpdateRanking();
+    void PlayerPosUpdate();
     int mWindowWidth;
     int mWindowHeight;
 
     SDL_Texture* mRankingUI[MAX_CLIENTS];
     SDL_Texture* mRacerUI[MAX_CLIENTS];
+    SDL_Texture *mPlayerPosUI[MAX_CLIENTS];
     SDL_Texture *mNoPlayerUI;
+    SDL_Texture *mMapUI;
+    SDL_Texture *mMapBaseUI;
 
     std::vector<class ItemComponent *> mItems;
 
     int mRank[MAX_CLIENTS] = {3,2,1,0};
+
+    Vector2 mPlPos[MAX_CLIENTS];
 
     std::vector<class Racer*> mRacers;
 };
